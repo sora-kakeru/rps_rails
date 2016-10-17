7d079da1dc3633e237c165acabb649d7ef70123e
Nobuhiro Kawai <nkawai@chicagobooth.edu>
2016-10-17 02:10:34 -0500

    homework

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,9 +12,24 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = ["rock","paper","scissors"].sample
 
-    @result = "Replace this string with the correct value."
+    @result =
+    if @user_move == @computer_move
+      "tie"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      "won"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      "lost"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      "lost"
+    else
+      "won"
+    end
 
     # ===============================================================
     # Your code goes above.
